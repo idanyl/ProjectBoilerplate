d1242ae2861d740671292787e565507e82517bc2
iDanyl <57048452+idanyl@users.noreply.github.com>
2019-10-27 10:36:36 +0200

    Add files via upload

---
+++ ProjectBoilerplate/ProjectBoilerplate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ProjectBoilerplate
+//
+//  Created by Danyl Timofeyev on 10/26/19.
+//  Copyright © 2019 Danyl Timofeyev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
